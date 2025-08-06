e1e51cd9905181bb12e46b80c8a9d6b89924e24b
shahrukhdev-ilm <shahrukh@ilmach.com>
2025-08-06 20:29:43 +0500

    Update offer.jsx

--- src/sections/offer.jsx
+++ src/sections/offer.jsx
@@ -15,7 +15,7 @@ export const Offer = () => {
   return (
     <div id='services' className='w-full h-auto flex flex-col items-center justify-center lg:px-[80px] py-[60px] gap-[20px]'>
       <h1 data-aos="zoom-in" data-aos-delay="50" className='text-themegreen uppercase text-sm font-poppins'>OUR SERVICES</h1>
-      <h1 data-aos="zoom-in" data-aos-delay="100" className='text-white lg:text-[70px] text-[40px] capitalize font-poppins leading-[1.2em] font-poppins'>Driving Digital Transformation with Excellence</h1>
+      <h1 data-aos="zoom-in" data-aos-delay="100" className='text-grey-500 lg:text-[70px] text-[40px] capitalize font-poppins leading-[1.2em] font-poppins'>Driving Digital Transformation with Excellence</h1>
       <p data-aos="zoom-in" data-aos-delay="150" className='text-grey-500 text-md font-poppins lg:w-[60%] w-full text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Sed viverra, enim in facilisis facilisis, enim erat commodo est, a tincidunt nisi ligula non quam.</p>
       <div className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-6 mt-6'>
         {services.map((service, index) => (
