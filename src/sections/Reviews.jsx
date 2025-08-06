8e35f852b4e24baf858d37e18aba50fe4845a7e9
shahrukhdev-ilm <shahrukh@ilmach.com>
2025-08-06 18:58:43 +0500

    Update Reviews.jsx
    url changes

--- src/sections/Reviews.jsx
+++ src/sections/Reviews.jsx
@@ -31,6 +31,31 @@ export const Reviews = () => {
         ))}
       </div>
     </div>
+    {/* Location Section */}
+      <div
+        id="location"
+        className="w-full flex flex-col items-center justify-center lg:px-[80px] px-[20px] py-[60px] gap-4"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <h2 className="text-themegreen uppercase text-sm font-poppins">Our Location</h2>
+        <p className="text-gray-500 text-md font-poppins text-center">
+          Visit us at: Intelligent Learning Machines, National Science and Technology Park, Islamabad, Pakistan
+        </p>
+        <div className="w-full lg:w-[60%] h-[300px] rounded-md overflow-hidden shadow-lg">
+          <iframe
+            title="Company Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13613.64662734146!2d72.991426!3d33.646301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df9573aecd2f93%3A0x1c7fdc5084512ca2!2sNational%20Science%20%26%20Technology%20Park%20(NSTP)!5e0!3m2!1sen!2s!4v1718035700000!5m2!1sen!2s"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </>
   )
 }
 
